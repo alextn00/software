4c1fa415ee4248c5dbe4ab74f48ffa461b83af3a
kcm0147 <57346393+kcm0147@users.noreply.github.com>
2019-12-04 22:34:41 +0900

    pd layer 2

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Student user = Student.getInstance();
-        counseling_history h = new counseling_history();
-        h.scan_nonSubjectActivity();
-        System.out.println(h.getCounseling_number());
+        System.out.println(user.getTrack());
+        //counseling_history h = new counseling_history();
+        //h.scan_nonSubjectActivity();
+        //System.out.println(h.getCounseling_number());
 
 
-        field_practice b = new field_practice();
+        //field_practice b = new field_practice();
 
-        graduation_requirement c = graduation_requirement.getInstance();
-        System.out.println(c.getCounseling());
+        //graduation_requirement c = graduation_requirement.getInstance();
+        //System.out.println(c.getCounseling());
 
-        System.out.println(b.getField_credit());
+        //System.out.println(b.getField_credit());
 
 
         nonSubjectActivity n = new nonSubjectActivity();
 
-        Data_nonSubject d = Data_nonSubject.getInstance();
+        //Data_nonSubject d = Data_nonSubject.getInstance();
         data_curriculum p = data_curriculum.getInstance();
 
-        Arrays.toString(p.getCredit());
+        System.out.println(Arrays.toString(p.getCurriculum_classification()));
+        System.out.println(p.getMajorbase_credit());
         System.out.println(p.getMajor_credit());
 
 
